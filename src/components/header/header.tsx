b0ea9e2c61c1fd9ca25997b31d8fa6ad8b38548a
Vinoth Sharma <vinoth.veeramani@mu-sigma.com>
2021-06-22 16:52:14 +0530

    header completed

---
+++ src/components/header/header.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+import "./header.scss";
+
+export function Header() {
+  return (
+    <header className="App-header">
+      <img src="icons/wallet.png" alt="" />
+      <span>Subscribe Me</span>
+    </header>
+  );
+}
